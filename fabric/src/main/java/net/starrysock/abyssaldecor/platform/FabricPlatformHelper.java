2b98b5e862ad1b2f180c3818a62b6d3feed5c1bc
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2025-07-26 18:06:04 -0400

    fix fabric side

--- fabric/src/main/java/net/starrysock/abyssaldecor/platform/FabricPlatformHelper.java
+++ fabric/src/main/java/net/starrysock/abyssaldecor/platform/FabricPlatformHelper.java
@@ -1,5 +1,8 @@
 package net.starrysock.abyssaldecor.platform;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import net.starrysock.abyssaldecor.platform.services.IPlatformHelper;
 import net.fabricmc.loader.api.FabricLoader;
 
@@ -27,5 +30,15 @@ public class FabricPlatformHelper implements IPlatformHelper {
     @Override
     public Path getConfigDirectory() {
         return FabricLoader.getInstance().getConfigDir();
+    }
+
+    @Override
+    public boolean onCropsGrowPre(Level level, BlockPos pos, BlockState state, boolean def) {
+        return false;
+    }
+
+    @Override
+    public void onCropsGrowPost(Level level, BlockPos pos, BlockState state) {
+
     }
 }
