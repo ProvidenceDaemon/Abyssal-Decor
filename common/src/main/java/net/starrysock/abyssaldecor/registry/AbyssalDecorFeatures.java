85ef142ac2c53ac52ec843d34adb5dd92104c4df
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2025-11-09 13:39:29 -0500

    recipe machine broke

---
+++ common/src/main/java/net/starrysock/abyssaldecor/registry/AbyssalDecorFeatures.java
@@ -0,0 +1,34 @@
+package net.starrysock.abyssaldecor.registry;
+
+import dev.architectury.registry.registries.DeferredRegister;
+import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.world.level.levelgen.feature.Feature;
+
+public class AbyssalDecorFeatures {
+    /*public static final DeferredRegister<Feature<?>> REGISTRY;
+    public static final RegistrySupplier<Feature<?>> BOG_APPLE_STRUCTURE_FEATURE;
+    public static final RegistrySupplier<Feature<?>> RIPE_BOG_APPLE_STRUCTURE_FEATURE;
+    public static final RegistrySupplier<Feature<?>> CLAM_STRUCTURE_FEATURE;
+    public static final RegistrySupplier<Feature<?>> CLAM_PEARL_STRUCTURE_FEATURE;
+    public static final RegistrySupplier<Feature<?>> SPIDERCORN_STRUCTURE_FEATURE;
+    public static final RegistrySupplier<Feature<?>> STARFISH_STRUCTURE_FEATURE;
+    public static final RegistrySupplier<Feature<?>> ELDER_BIRCH_RUIN_STRUCTURE_FEATURE;
+    public static final RegistrySupplier<Feature<?>> MUCKROOT_STRUCTURE_FEATURE;
+    public static final RegistrySupplier<Feature<?>> AMARANTH_STRUCTURE_FEATURE;
+
+    public AbyssalDecorFeatures() {
+    }
+
+    static {
+        REGISTRY = DeferredRegister.create(ForgeRegistries.FEATURES, "abyssal_decor");
+        BOG_APPLE_STRUCTURE_FEATURE = REGISTRY.register("bog_apple_structure_feature", BogAppleStructureFeatureFeature::new);
+        RIPE_BOG_APPLE_STRUCTURE_FEATURE = REGISTRY.register("ripe_bog_apple_structure_feature", RipeBogAppleStructureFeatureFeature::new);
+        CLAM_STRUCTURE_FEATURE = REGISTRY.register("clam_structure_feature", ClamStructureFeatureFeature::new);
+        CLAM_PEARL_STRUCTURE_FEATURE = REGISTRY.register("clam_pearl_structure_feature", ClamPearlStructureFeatureFeature::new);
+        SPIDERCORN_STRUCTURE_FEATURE = REGISTRY.register("spidercorn_structure_feature", SpidercornStructureFeatureFeature::new);
+        STARFISH_STRUCTURE_FEATURE = REGISTRY.register("starfish_structure_feature", StarfishStructureFeatureFeature::new);
+        ELDER_BIRCH_RUIN_STRUCTURE_FEATURE = REGISTRY.register("elder_birch_ruin_structure_feature", ElderBirchRuinStructureFeatureFeature::new);
+        MUCKROOT_STRUCTURE_FEATURE = REGISTRY.register("muckroot_structure_feature", MuckrootStructureFeatureFeature::new);
+        AMARANTH_STRUCTURE_FEATURE = REGISTRY.register("amaranth_structure_feature", AmaranthStructureFeatureFeature::new);
+    }*/
+}
